76a67747ab4fc3949814748adf3b933277b0f127
Aditya Prawira <adityaprawira96@gmail.com>
2021-02-15 18:42:40 +1100

    Added SongList inner class in Album class and modify some methodas in the Album class

--- src/InterAbs/Playlist/MainPlaylist.java
+++ src/InterAbs/Playlist/MainPlaylist.java
@@ -6,20 +6,11 @@ public class MainPlaylist {
     private static ArrayList<Album> albums = new ArrayList<Album>();
     public static void main(String[] args) {
         /*
-        1. Creating program that implements a playlist of a song
-        2. Create a Song class that has title and duraiton attributes
-        3. The program will have ALbum class which will contain list of song
-        4. Song from different Albums can be added to the playlist and will appear
-           in the list in order they are added
-        5. When the songs have been added to the playlist, create a menu options
-                a. quit
-                b. skip to next song
-                c. skip to previous song
-                d. replay the current song
-                e. List of the songs in the playlist
-         6. A song must exist in an album before it can be added to the playlist
-         7. Remove song from playlist
-         */
+        * Modify the Album class that uses an inner class.
+        * Instead of utilizing ArrayList to hold its tracks, create an inner class called SongList.
+        * The inner SongList class will use an ArrayList and will provide a method to add a song.
+        * Neither the Song class or the Main class should be changed.
+        */
 
         Album album = new Album("Within the Mist", "Azure Gaze");
         album.addSong("within the mist", 2.13);
