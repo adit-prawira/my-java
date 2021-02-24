9e3a1da2ddb4eed016ba99c980c438ba6cc0147f
Aditya Prawira <adityaprawira96@gmail.com>
2021-02-24 16:15:58 +1100

    Added planets and moons to solar system model

--- src/SolarSystem/HeavenlyBody.java
+++ src/SolarSystem/HeavenlyBody.java
@@ -1,5 +1,32 @@
 package SolarSystem;
 
-public class HeavenlyBody {
+import java.util.HashSet;
+import java.util.Set;
 
+// Immutable
+public class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satellites;
+
+    public HeavenlyBody(String name, double orbitalPeriod) {
+        this.name = name;
+        this.orbitalPeriod = orbitalPeriod;
+        this.satellites = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public Set<HeavenlyBody> getSatellites() {
+        return new HashSet<>(this.satellites);
+    }
+    public boolean addMoon(HeavenlyBody moon){
+        return this.satellites.add(moon);
+    }
 }
--- src/SolarSystem/MainSolarSystem.java
+++ src/SolarSystem/MainSolarSystem.java
@@ -1,9 +1,94 @@
 package SolarSystem;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class MainSolarSystem {
+    private static Map<String, HeavenlyBody> solarSystems = new HashMap<>();
+    private static Set<HeavenlyBody> planets = new HashSet<>();
+
     public static void main(String[] args) {
         // Solar System
         // Has groups of Planets, Asteroids, Comets, Moons
         // Some moons might belong to another group
+
+        // Mercury
+        HeavenlyBody temp = new HeavenlyBody("Mercury", 88);
+        solarSystems.put(temp.getName(), temp);
+        planets.add(temp);
+
+        // Venus
+        temp = new HeavenlyBody("Venus", 225);
+        solarSystems.put(temp.getName(), temp);
+        planets.add(temp);
+
+        // Earth
+        temp = new HeavenlyBody("Earth", 365);
+        solarSystems.put(temp.getName(), temp);
+        planets.add(temp);
+
+        HeavenlyBody tempMoon = new HeavenlyBody("Moon", 27);
+        solarSystems.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // Add moon to temp which is current earth
+
+        // Mars
+        temp = new HeavenlyBody("Mars", 687);
+        solarSystems.put(temp.getName(), temp);
+        planets.add(temp);
+        tempMoon = new HeavenlyBody("Deimos", 1.3);
+        solarSystems.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon);
+        tempMoon = new HeavenlyBody("Phobos", 0.3);
+        solarSystems.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon);
+
+        // Jupiter
+        temp = new HeavenlyBody("Jupiter", 4332);
+        solarSystems.put(temp.getName(), temp);
+        planets.add(temp);
+        tempMoon = new HeavenlyBody("Io", 1.8);
+        solarSystems.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon);
+        tempMoon = new HeavenlyBody("Europa", 3.5);
+        solarSystems.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon);
+        tempMoon = new HeavenlyBody("Ganymede", 7.1);
+        solarSystems.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon);
+        tempMoon = new HeavenlyBody("Callisto", 16.7);
+        solarSystems.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon);
+
+        // Saturn
+        temp = new HeavenlyBody("Saturn", 10759);
+        solarSystems.put(temp.getName(), temp);
+        planets.add(temp);
+
+        // Uranus
+        temp = new HeavenlyBody("Uranus", 30660);
+        solarSystems.put(temp.getName(), temp);
+        planets.add(temp);
+
+        // Neptune
+        temp = new HeavenlyBody("Neptune", 165);
+        solarSystems.put(temp.getName(), temp);
+        planets.add(temp);
+
+        // Pluto
+        temp= new HeavenlyBody("Pluto", 248);
+        solarSystems.put(temp.getName(), temp);
+        planets.add(temp);
+
+        System.out.println("Planets:");
+        for(HeavenlyBody planet: planets){
+            System.out.println("\t- " + planet.getName());
+        }
+        HeavenlyBody body = solarSystems.get("Jupiter");
+        System.out.println("Moons of " + body.getName());
+        for(HeavenlyBody satellite: body.getSatellites()){
+            System.out.println("\t- " + satellite.getName());
+        }
     }
 }
